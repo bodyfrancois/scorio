1a5b9456b1be23e879cea51039b7dcb10fd1b6ba
BODY <bodyfrancois@gmail.com>
2026-05-20 21:53:41 +0200

    feat: work in progress and improvements

--- src/games/registry.ts
+++ src/games/registry.ts
@@ -9,6 +9,9 @@ import { hiloConfig } from './hilo/config';
 import { skyjoConfig } from './skyjo/config';
 import { ligrettoConfig } from './ligretto/config';
 import { sixquiprendConfig } from './6quiprend/config';
+import { piliPiliConfig } from './pili_pili/config';
+import { fiveKingConfig } from './5_rois/config';
+import { diceConfig } from './dice/config';
 import { GameConfig } from '../core/types';
 
 export const ALL_GAMES: GameConfig[] = [
@@ -23,6 +26,9 @@ export const ALL_GAMES: GameConfig[] = [
   ligrettoConfig,
   sixquiprendConfig,
   speedbacConfig,
+  piliPiliConfig,
+  fiveKingConfig,
+  diceConfig,
 ];
 
 export function getGameConfig(name: string): GameConfig | undefined {
